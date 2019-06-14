6ed8f245ef92de0184abae91dde3e0099fb695c2
Atharva Sharma <mailatharvasharma@gmail.com>
2019-06-14 10:36:40 +0530

    card thank you

--- src/components/thankYou.vue
+++ src/components/thankYou.vue
@@ -1,19 +1,30 @@
 <template>
   <v-container grid-list-md>
-    <v-layout row wrap justify-center class="text-xs-center">
-    <v-flex xs12 md6 sm6 lg6>
-      <v-card dark color="#191C21" style="color:#B9BCC1;">
+    <v-layout column wrap justify-center class="text-xs-center">
+    <v-flex xs12 md6 sm6 lg6 class="hidden-sm-and-down">
+      <v-card dark color="#191C21" style="color:#B9BCC1; top:-70px;" height="100%">
         <v-card-text class="headline font-weight-bold display-4 text-xs-center">
           <span style="color:#B9BCC1; margin:20px;">Congratulations, your booking has been successfully made.</span>
         </v-card-text>
         <v-card-text class="headline font-weight-bold display-4 text-xs-center">
           <span style="color:#B9BCC1; margin:20px;"> Thank you for booking with us. We look forward to hosting you.</span>
         </v-card-text>
-        <router-link to="/">
-          <v-btn flat outline style="border: 1px solid #FFFFFF; box-sizing: border-box; box-shadow: 0px 9px 24px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 4px; max-width:340px; text-transform:capitalize">
+          <v-btn flat to="/" class="mb-3" outline style="border: 1px solid #FFFFFF; box-sizing: border-box; box-shadow: 0px 9px 24px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 4px; max-width:340px; text-transform:capitalize">
             Go Back Home
           </v-btn>
-        </router-link>
+      </v-card>
+    </v-flex>
+    <v-flex xs12 md6 sm6 lg6 class="hidden-md-and-up">
+      <v-card dark color="#191C21" style="color:#B9BCC1; margin-bottom:10px; margin-top:20px;" height="100%">
+        <v-card-text class="headline font-weight-bold display-4 text-xs-center">
+          <span style="color:#B9BCC1; margin:20px;">Congratulations, your booking has been successfully made.</span>
+        </v-card-text>
+        <v-card-text class="headline font-weight-bold display-4 text-xs-center">
+          <span style="color:#B9BCC1; margin:20px;"> Thank you for booking with us. We look forward to hosting you.</span>
+        </v-card-text>
+          <v-btn flat to="/" class="mb-3" outline style="border: 1px solid #FFFFFF; box-sizing: border-box; box-shadow: 0px 9px 24px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 4px; max-width:340px; text-transform:capitalize">
+            Go Back Home
+          </v-btn>
       </v-card>
     </v-flex>
     </v-layout>
