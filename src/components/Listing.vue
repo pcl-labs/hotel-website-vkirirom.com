9fc5e618a94b50e62d9e90529ff7e6f90fa72b21
Atharva Sharma <mailatharvasharma@gmail.com>
2019-06-29 12:41:08 +0530

    container padding:0

--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -760,16 +760,19 @@ input:-webkit-autofill {
   @media only screen and (max-width: 600px) {
     .container{
       max-width: 292px;
+      padding: 0;
     }
   }
   @media only screen and (min-width: 768px) {
     .container{
       max-width: 600px;
+      padding: 0;
     }
   }
   @media only screen and (min-width: 1024px) {
     .container{
       max-width: 900px;
+      padding: 0;
     }
   }
 </style>
--- src/components/searchpage.vue
+++ src/components/searchpage.vue
@@ -72,16 +72,19 @@ export default {
   @media only screen and (max-width: 600px) {
     .container{
       max-width: 292px;
+      padding: 0;
     }
   }
   @media only screen and (min-width: 768px) {
     .container{
       max-width: 600px;
+      padding: 0;
     }
   }
   @media only screen and (min-width: 1024px) {
     .container{
       max-width: 900px;
+      padding: 0;
     }
   }
 </style>
