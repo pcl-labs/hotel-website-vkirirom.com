dbdf2d8e160b37e77fbb8211fc2b2bbc11be7261
Atharva Sharma <mailatharvasharma@gmail.com>
2019-07-04 12:58:47 +0530

    bed types

--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -520,42 +520,6 @@
                 </v-layout>
                 <input name="Amount (in $)" hidden :value="finalPrice" type="text" readonly />
               </v-flex>
-              <v-layout row wrap justify-center justify-space-between v-if="resort.modules.hotel.beds && resort.modules.hotel.beds.length >0">
-                <v-flex style="margin-bottom:30px;">
-                  <v-btn-toggle v-model="toggle0" mandatory style="background-color:transparent; width:100%;">
-                    <v-btn v-if="resort.modules.hotel.beds.length >0" :value="resort.modules.hotel.beds[0].type" flat dark outline block text-xs-center class="text-capitalize" style="border: 3px solid #B9BCC1; box-sizing: border-box; border-radius: 3px; height:108px; font-weight: bold; font-size: 14px; line-height: 17px; color: #B9BCC1;">
-                      <p class="mt-4">
-                        {{ resort && resort.modules && resort.modules.hotel && resort.modules.hotel.beds[0] && resort.modules.hotel.beds[0].count }}
-                        {{ resort && resort.modules && resort.modules.hotel && resort.modules.hotel.beds[0] && resort.modules.hotel.beds[0].type }}
-                      <br>
-                        <v-img class="mt-1 ml-3" width="22px" height="20px" :src="bed"></v-img>
-                      </p>
-                    </v-btn>
-                    <v-btn v-if="resort.modules.hotel.beds.length >1" :value="resort.modules.hotel.beds[1].type" flat dark outline block text-xs-center class="text-capitalize" style="margin-left:20px; border: 3px solid #B9BCC1; box-sizing: border-box; border-radius: 3px; height:108px; font-weight: bold; font-size: 14px; line-height: 17px; color: #B9BCC1;">
-                      <p class="mt-4">  
-                        {{ resort && resort.modules && resort.modules.hotel && resort.modules.hotel.beds[1] && resort.modules.hotel.beds[1].count }}
-                        {{ resort && resort.modules && resort.modules.hotel && resort.modules.hotel.beds[1] && resort.modules.hotel.beds[1].type }}
-                      <br>
-                        <v-img class="mt-1 ml-3" width="22px" height="20px" :src="bed"></v-img>
-                      </p>
-                    </v-btn>
-                  </v-btn-toggle>
-                </v-flex>
-              </v-layout>
-              <input
-                name="Type"
-                hidden
-                :value="toggle0"
-                type="text"
-                readonly
-              />
-              <!-- <input
-                name="Type2"
-                hidden
-                :value="toggle0"
-                type="text"
-                readonly
-              /> -->
               <v-flex xs12>
                 <v-textarea
                   outline
