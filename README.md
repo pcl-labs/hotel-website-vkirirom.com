d37ee7ac9f1269bd68a373f22ccf6c1f905a7b88
Morteza Ziyae <m.ziaeemehr@gmail.com>
2019-07-19 19:44:23 +0430

    clean babel config, fix readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vkirirom
+# vkirirom.com_v2
 
 ## Project setup
 ```
--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ],
-  env: {
-    test: {
-      presets: ["env", "stage-2"],
-      plugins: [ "istanbul" ]
-    }
-  }
+  ]
 }
