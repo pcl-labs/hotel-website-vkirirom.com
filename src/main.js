0aec1bac04b917eab98b71ceade61b83a9ba0bfb
Morteza Ziyae <m.ziaeemehr@gmail.com>
2019-07-19 17:47:33 +0430

    undo vuetify changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import './plugins/vuetify'
 import App from './App'
 import Vuetify from 'vuetify'
 import 'vuetify/src/stylus/app.styl'
