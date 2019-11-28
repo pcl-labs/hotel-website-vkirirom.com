3e43a41579e1aa90eeb284cbdd8ef0a6234a01ff
Morteza Ziyae <m.ziaeemehr@gmail.com>
2019-11-28 20:14:35 +0330

    vuetify - layout of confirm pay page

--- src/components/Reservation/ConfirmAndPay.vue
+++ src/components/Reservation/ConfirmAndPay.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container grid-list-md>
+  <v-container class="is-limited">
     <v-row no-gutters style="min-height:100vh;">
       <v-flex xs12 md7 class="text">
         <v-row no-gutters>
@@ -47,7 +47,8 @@
           </v-flex>
           <v-flex xs12>
             <v-btn
-              class="button"
+              x-large
+              class="button text-capitalize"
               color="#F7B947"
               v-on:click="purchase"
             >
@@ -188,38 +189,5 @@ export default {
   .bookingCard{
     margin-top:30px;
     margin-bottom:30px;
-  }
-
-  @-webkit-keyframes autofill {
-    to {
-        color: white;
-        background: transparent;
-    }
-  }
-  input:-webkit-autofill {
-      -webkit-animation-name: autofill;
-      -webkit-animation-fill-mode: both;
-  }
-  .container{
-    padding-top: 30px;
-    padding-left: 0px;
-    padding-right: 0px;
-    // doing padding:0px or  0; or padding-bottom:0px or 0; creates an extra overflow scroll which is bad and should be avoided
-  }
-
-  @media only screen and (max-width: 600px) {
-    .container{
-      max-width: 292px;
-    }
-  }
-  @media only screen and (min-width: 768px) {
-    .container{
-      max-width: 600px;
-    }
-  }
-  @media only screen and (min-width: 1024px) {
-    .container{
-      max-width: 900px;
-    }
   }
 </style>
