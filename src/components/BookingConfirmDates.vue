5244e3f78a556326d2e86f77ae03f6963070dc0d
Morteza Ziyae <m.ziaeemehr@gmail.com>
2019-12-04 09:19:00 +0330

    booking flow 2 - update prices and date selection

--- src/components/BookingConfirmDates.vue
+++ src/components/BookingConfirmDates.vue
@@ -2,6 +2,12 @@
   <div class="light--text mx-auto">
     <v-card color="dark px-2 pb-4" tile :ripple="false">
       <input
+        type="hidden"
+        name="form-name"
+        :value="resort.categories[0].name"
+      />
+
+      <input
         id="datepicker-inline-trigger"
         :rules="dateRules"
         :value="formatDates(dateOne, dateTwo)"
@@ -22,46 +28,71 @@
         :date-two="dateTwo"
         @date-one-selected="
           val => {
-            dateOne = val
-            dateTwo = val
+            updateDateOne(val)
+            updateDateTwo(val)
             getPrices(dateOne, dateTwo)
           }
         "
         @date-two-selected="
           val => {
-            dateTwo = val
+            updateDateTwo(val)
             getPrices(dateOne, dateTwo)
           }
         "
       ></airbnb-style-datepicker>
 
-      <div class="confirm-dates--prices-list px-4">
-        <v-row no-gutters v-for="price in prices" v-bind:key="price.id">
-          <v-flex xs6 class="normalText">{{ formatDates(price.date) }}</v-flex>
-          <v-flex xs6 class="text-right normalText">${{ price.amount }}</v-flex>
+      <div class="px-4 light--text">
+        <v-row no-gutters class="mb-6 title">
+          <v-col xs6>{{ prices.length }} nights total</v-col>
         </v-row>
-        <v-row no-gutters>
-          <v-flex XS6 class="normalText">VAT (10%)</v-flex>
-          <v-flex XS6 class="text-right normalText">${{ vat }}</v-flex>
-        </v-row>
-        <v-divider
-          style="background-color:#3D424E; margin-top:20px; margin-bottom:10px;"
-        ></v-divider>
-        <v-row no-gutters>
-          <v-flex xs6>
-            <h3 style="font-size: 20px; color: #D8DADE;">Total</h3>
-          </v-flex>
-          <v-flex xs6 class="text-right">
-            <h3 style="font-size: 20px; color: #D8DADE;">${{ finalPrice }}</h3>
-          </v-flex>
-        </v-row>
-        <input
-          name="Amount (in $)"
-          hidden
-          :value="finalPrice"
-          type="text"
-          readonly
-        />
+        <div class="confirm-dates--prices-list">
+          <div class="mb-4 body-2 font-weight-light">
+            <v-row no-gutters v-for="price in prices" v-bind:key="price.id">
+              <v-col xs6>{{ formatDates(price.date) }}</v-col>
+              <v-col xs6 class="text-right ">${{ price.amount }}</v-col>
+            </v-row>
+            <v-row no-gutters>
+              <v-col xs6>VAT (10%)</v-col>
+              <v-col xs6 class="text-right ">${{ vat }}</v-col>
+            </v-row>
+          </div>
+          <v-divider
+            class="mb-4 gray-4f"
+          ></v-divider>
+          <v-row no-gutters class="mb-6">
+            <v-col xs6>
+              <h3 class="title">Total</h3>
+            </v-col>
+            <v-col xs6 class="text-right">
+              <h3 class="title">${{ finalPrice }}</h3>
+            </v-col>
+          </v-row>
+          <input
+            name="Amount (in $)"
+            hidden
+            :value="finalPrice"
+            type="text"
+            readonly
+          />
+        </div>
+      </div>
+
+      <div class="px-4">
+        <v-btn
+          @click="submit"
+          x-large
+          block
+          color="primary"
+          dark
+          class="text-capitalize font-weight-bold dark--text"
+          :disabled="!valid"
+          type="submit"
+        >
+          <v-spacer></v-spacer>
+          <span>Confirm Dates</span>
+          <v-spacer></v-spacer>
+          <v-icon>keyboard_arrow_right</v-icon>
+        </v-btn>
       </div>
     </v-card>
   </div>
@@ -76,28 +107,19 @@ export default Vue.extend({
   name: 'booking-confirm-dates',
   data() {
     return {
-      dateRules: [v => !!v || 'Dates are required']
+      dateRules: [v => !!v || 'Dates are required'],
+      valid: false
     }
   },
   computed: {
     resort() {
       return this.$store.getters['resort/getResort']
     },
-    dateOne: {
-      get() {
-        return store.getters['booking/bookingInfo'].dateOne
-      },
-      set(value) {
-        store.dispatch('booking/updateDateOne', value)
-      }
+    dateOne() {
+      return store.getters['booking/bookingInfo'].dateOne
     },
-    dateTwo: {
-      get() {
-        return store.getters['booking/bookingInfo'].dateTwo
-      },
-      set(value) {
-        store.dispatch('booking/updateDateTwo', value)
-      }
+    dateTwo() {
+      return store.getters['booking/bookingInfo'].dateTwo
     },
     prices() {
       return this.$store.getters['reservation/prices']
@@ -111,6 +133,12 @@ export default Vue.extend({
   },
   methods: {
     formatDates,
+    updateDateOne(val) {
+      store.dispatch('booking/updateDateOne', val)
+    },
+    updateDateTwo(val) {
+      store.dispatch('booking/updateDateTwo', val)
+    },
     getPrices() {
       const dateOne = this.dateOne
       const dateTwo = this.dateTwo
@@ -120,6 +148,9 @@ export default Vue.extend({
         dateOne,
         dateTwo
       })
+    },
+    submit() {
+      store.dispatch('booking/updateCurrentStep', 2)
     }
   }
 })
