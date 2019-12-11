4f7c4efe1b957110ae687f1abc570e0bcd9da09d
Morteza Ziyae <m.ziaeemehr@gmail.com>
2019-12-11 21:50:52 +0330

    booking flow 4 - add guests

--- src/components/BookingConfirmGuests.vue
+++ src/components/BookingConfirmGuests.vue
@@ -12,16 +12,163 @@
         <v-icon color="gray-82">close</v-icon>
       </v-btn>
       <v-toolbar-title class="light--text pl-0 ml-n4 text-center"
-        >Confirm Guests [WIP]</v-toolbar-title
+        >Guests</v-toolbar-title
       >
     </v-toolbar>
 
     <div class="d-flex flex-column">
-      <div class="light--text mx-auto">
-        <v-card color="dark px-2 pb-4" tile :ripple="false">
-          this is confirm guests
-        </v-card>
-      </div>
+      <v-card color="dark px-2 pb-4 light--text" tile :ripple="false">
+        <v-form v-model="isFormValid">
+          <v-text-field
+            class="d-none"
+            :value="guestsTotal"
+            type="text"
+            readonly
+            :rules="rules.guestsTotal"
+          />
+          <v-list dark>
+            <v-list-item>
+              <v-list-item-content class="light--text">
+                <v-list-item-title class="font-weight-bold body-1 light--text"
+                  >Adult</v-list-item-title
+                >
+              </v-list-item-content>
+
+              <v-list-item-icon>
+                <div class="d-flex light--text align-center">
+                  <v-btn
+                    :disabled="guestsAdults <= 0"
+                    @click="guestsAdults--"
+                    color="light"
+                    x-small
+                    fab
+                    icon
+                    outlined
+                    ><v-icon>remove</v-icon></v-btn
+                  >
+                  <span
+                    class="mx-4 text--light guests-count user-select-none text-center"
+                    >{{ guestsAdults }}</span
+                  >
+                  <v-btn
+                    @click="guestsAdults++"
+                    color="light"
+                    x-small
+                    fab
+                    icon
+                    outlined
+                    ><v-icon>add</v-icon></v-btn
+                  >
+                </div>
+              </v-list-item-icon>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-content class="light--text">
+                <v-list-item-title class="font-weight-bold body-1"
+                  >Children</v-list-item-title
+                >
+                <v-list-item-subtitle>Age 2 - 12</v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-icon>
+                <div class="d-flex light--text align-center">
+                  <v-btn
+                    :disabled="guestsChildren <= 0"
+                    @click="guestsChildren--"
+                    color="light"
+                    x-small
+                    fab
+                    icon
+                    outlined
+                    ><v-icon>remove</v-icon></v-btn
+                  >
+                  <span
+                    class="mx-4 text--light guests-count user-select-none text-center"
+                    >{{ guestsChildren }}</span
+                  >
+                  <v-btn
+                    @click="guestsChildren++"
+                    color="light"
+                    x-small
+                    fab
+                    icon
+                    outlined
+                    ><v-icon>add</v-icon></v-btn
+                  >
+                </div>
+              </v-list-item-icon>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-content class="light--text">
+                <v-list-item-title class="font-weight-bold body-1"
+                  >Infant</v-list-item-title
+                >
+                <v-list-item-subtitle>Under 2</v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-icon>
+                <div class="d-flex light--text align-center">
+                  <v-btn
+                    :disabled="guestsInfants <= 0"
+                    @click="guestsInfants--"
+                    color="light"
+                    x-small
+                    fab
+                    icon
+                    outlined
+                    ><v-icon>remove</v-icon></v-btn
+                  >
+                  <span
+                    class="mx-4 text--light guests-count user-select-none text-center"
+                    >{{ guestsInfants }}</span
+                  >
+                  <v-btn
+                    @click="guestsInfants++"
+                    color="light"
+                    x-small
+                    fab
+                    icon
+                    outlined
+                    ><v-icon>add</v-icon></v-btn
+                  >
+                </div>
+              </v-list-item-icon>
+            </v-list-item>
+          </v-list>
+
+          <div class="mx-4">
+            <v-divider class="light-border my-8"></v-divider>
+
+            <!-- total -->
+            <v-row no-gutters class="mb-4">
+              <v-col xs6>
+                <h3 class="title">Total</h3>
+              </v-col>
+              <v-col xs6 class="text-right">
+                <h3 class="title">{{ guestsTotal }} Guests</h3>
+              </v-col>
+            </v-row>
+
+            <v-btn
+              @click="submit"
+              x-large
+              block
+              color="primary"
+              dark
+              class="text-transform-none font-weight-bold dark--text"
+              :disabled="!isFormValid"
+              type="submit"
+            >
+              <v-spacer></v-spacer>
+              <span>Confirm Guests</span>
+              <v-spacer></v-spacer>
+              <v-icon>keyboard_arrow_right</v-icon>
+            </v-btn>
+          </div>
+        </v-form>
+      </v-card>
     </div>
   </v-card>
 </template>
@@ -39,10 +186,25 @@ export default Vue.extend({
     }
   },
   data() {
-    return {}
+    return {
+      isFormValid: false,
+      guestsAdults: 0,
+      guestsChildren: 0,
+      guestsInfants: 0,
+      rules: {
+        guestsTotal: [v => v > 0 || 'Specify the number of guests']
+      }
+    }
   },
-  computed: {},
+  computed: {
+    guestsTotal(): number {
+      return this.guestsAdults + this.guestsChildren + this.guestsInfants
+    }
+  },
   methods: {
+    submit() {
+      this.goNextStep()
+    },
     goNextStep() {
       store.dispatch('booking/updateCurrentStep', this.nextStep)
     }
@@ -50,4 +212,13 @@ export default Vue.extend({
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/styles/utility.scss';
+
+.v-list.theme--dark {
+  background: $dark;
+}
+.guests-count {
+  min-width: rem(24px);
+}
+</style>
