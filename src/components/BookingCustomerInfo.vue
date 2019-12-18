6d25465a30579ffc61962bfea841137c9bfc2d30
Morteza Ziyae <m.ziaeemehr@gmail.com>
2019-12-18 21:58:54 +0330

    booking flow 7 - add countries list in phone field

--- src/components/BookingCustomerInfo.vue
+++ src/components/BookingCustomerInfo.vue
@@ -2,7 +2,7 @@
   <div>
     <v-row no-gutters class="mb-8">
       <v-col>
-        <v-form v-model="isFormValid" @submit.prevent="">
+        <v-form v-model="isFormValid" @submit.prevent>
           <h1
             class="my-6 font-weight-bold"
             :class="{ 'display-1': $vuetify.breakpoint.smAndDown, 'display-2': $vuetify.breakpoint.mdAndUp }"
@@ -10,15 +10,43 @@
             Customer Info
           </h1>
           <h4 class="mb-2 title font-weight-bold">Guest Info</h4>
-          <v-text-field
-            v-model="phone"
-            v-mask="'+###-####-####'"
-            type="text"
-            dark=""
-            outlined=""
-            label="Phone Number"
-            hint="e.g. +855..."
-          ></v-text-field>
+          <v-row dense no-gutters class="phone-input align-center">
+            <v-col cols="5">
+              <v-combobox
+                v-model="phoneCountry"
+                class="phone-input--code"
+                item-color="dark"
+                color="light"
+                dark
+                :items="CountriesList"
+                item-text="name"
+                auto-select-first
+                label="Country code"
+                :suffix="(phoneCountry && `+${phoneCountry.callingCodes[0]}`) || ''"
+                outlined
+              >
+                <template v-if="phoneCountry" slot="prepend-inner"
+                  ><span class="d-flex"
+                    ><img
+                      class="phone-input--flag"
+                      v-if="phoneCountry && phoneCountry.flag"
+                      :src="phoneCountry.flag"
+                      alt=""/></span
+                ></template>
+              </v-combobox>
+            </v-col>
+            <v-col cols="7">
+              <v-text-field
+                class="phone-input--number"
+                v-model="phone"
+                v-mask="'+###-####-####'"
+                type="text"
+                dark
+                outlined
+                label="Phone Number"
+              ></v-text-field>
+            </v-col>
+          </v-row>
 
           <h4 class="mb-2 title font-weight-bold">Transportation</h4>
 
@@ -31,7 +59,8 @@
             <v-card outlined color="transparent" class="mb-2" @click="transportation = 2">
               <v-card-title class="justify-space-between">
                 <v-radio color="green" label="Shuttle Bus ($10/Pax)" :value="2" class="ma-0"></v-radio>
-                <a @click.stop="" target="_blank" href="https://example.com" class="body-1 text-decoration-none"
+                <!-- TODO: modal -->
+                <a @click.stop="" target="_blank" href="https://vkirirom.com/listing/Shuttle-Bus/" class="body-1 text-decoration-none"
                   >More Info</a
                 >
               </v-card-title>
@@ -65,6 +94,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import { mask } from 'vue-the-mask'
+import CountriesList from '@/constants/countries-list'
 
 export default Vue.extend({
   name: 'booking-customer-info',
@@ -73,9 +103,11 @@ export default Vue.extend({
   },
   data() {
     return {
+      phoneCountry: undefined,
       isFormValid: false,
       phone: '',
-      transportation: 1
+      transportation: 1,
+      CountriesList
     }
   },
   methods: {
@@ -91,7 +123,36 @@ export default Vue.extend({
 .theme--dark.v-card.v-card--outlined {
   border-color: $light !important;
 }
-::v-deep .theme--dark.v-icon {
-  color: $light;
+::v-deep {
+  .phone-input--code {
+    .v-label--active + input[type='text'] {
+      margin-left: rem(24px);
+    }
+    .v-input__slot {
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+    }
+  }
+  .phone-input--number .v-input__slot {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    margin-left: -1px;
+  }
+  .phone-input--flag {
+    width: rem(16px);
+    height: rem(16px);
+  }
+  .phone-input {
+    .v-input__prepend-inner {
+      align-self: center;
+      // margin: 0 8px 0 0;
+      margin: 0;
+      padding: 0;
+      width: 0;
+    }
+  }
+  .theme--dark.v-icon {
+    color: $light;
+  }
 }
 </style>
