d3091dbd1381c227fd26c2e87f927be6dad7149e
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-01-06 00:18:50 +0330

    home - use passive events (performance), add types, responsive

--- src/components/PageHomeParrallaxHero.vue
+++ src/components/PageHomeParrallaxHero.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="wrapper">
-    <div id="parallax-container">
+    <div class="parallax-container" ref="parallaxContainer">
       <div
         class="layer layer-1"
-        style="background: url(https://res.cloudinary.com/die9ji2vn/image/upload/v1578237751/website-static/page-home/1_rsfcu3.png);"
+        style="background-image: url(https://res.cloudinary.com/die9ji2vn/image/upload/v1578237751/website-static/page-home/1_rsfcu3.png);"
       ></div>
       <!-- <div
         id="meredith"
-        style="background: url(https://res.cloudinary.com/whynotearth/image/upload/c_scale,q_auto:low,f_auto/v1562682607/meredith_fkpe6f.png); background-repeat: no-repeat; top:90px;"
+        style="background-image: url(https://res.cloudinary.com/whynotearth/image/upload/c_scale,q_auto:low,f_auto/v1562682607/meredith_fkpe6f.png); background-repeat: no-repeat; top:90px;"
       >
         <v-layout justify-center>
           <v-flex xs12 sm6 md6 text-xs-center class="headerText">
@@ -19,23 +19,23 @@
 
       <div
         class="layer layer-2"
-        style="background: url(https://res.cloudinary.com/die9ji2vn/image/upload/v1578237752/website-static/page-home/2_h2p1ef.png);"
+        style="background-image: url(https://res.cloudinary.com/die9ji2vn/image/upload/v1578237752/website-static/page-home/2_h2p1ef.png);"
       ></div>
       <div
         class="layer layer-3"
-        style="background: url(https://res.cloudinary.com/die9ji2vn/image/upload/v1578237729/website-static/page-home/3_hsdxat.png);"
+        style="background-image: url(https://res.cloudinary.com/die9ji2vn/image/upload/v1578237729/website-static/page-home/3_hsdxat.png);"
       ></div>
       <div
         class="layer layer-4"
-        style="background: url(https://res.cloudinary.com/die9ji2vn/image/upload/v1578237689/website-static/page-home/4_hjrg9j.png);"
+        style="background-image: url(https://res.cloudinary.com/die9ji2vn/image/upload/v1578237689/website-static/page-home/4_hjrg9j.png);"
       ></div>
       <div
         class="layer layer-5"
-        style="background: url(https://res.cloudinary.com/die9ji2vn/image/upload/v1578237745/website-static/page-home/5_znwlz3.png);"
+        style="background-image: url(https://res.cloudinary.com/die9ji2vn/image/upload/v1578237745/website-static/page-home/5_znwlz3.png);"
       ></div>
       <div
         class="layer layer-6"
-        style="background: url(https://res.cloudinary.com/die9ji2vn/image/upload/v1578237682/website-static/page-home/6_oxiucy.png);"
+        style="background-image: url(https://res.cloudinary.com/die9ji2vn/image/upload/v1578237682/website-static/page-home/6_oxiucy.png);"
       ></div>
     </div>
   </div>
@@ -43,12 +43,10 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import { getPassiveEventConfig } from '@/helpers'
 
 export default Vue.extend({
   name: 'page-home-parrallax-hero',
-  data() {
-    return {}
-  },
   mounted() {
     this.init()
   },
@@ -58,27 +56,20 @@ export default Vue.extend({
       window.addEventListener(
         'scroll',
         () => {
-          let parent = document.getElementById('parallax-container')
-          let children = document.getElementsByClassName('layer')
+          let parent = this.$refs.parallaxContainer as HTMLElement
+          let children = parent.getElementsByClassName('layer') as HTMLCollectionOf<HTMLElement>
           // let meredith = document.getElementById('meredith') as HTMLElement
           // console.log('meredith', meredith);
 
           // meredith.style.transform = 'translateY(-' + window.pageYOffset * 0.3 + 'px)'
           for (let i = 0; i < children.length; i++) {
-            // @ts-ignore
-            children[i].style.transform = 'translateY(-' + (window.pageYOffset * i) / children.length + 'px)'
+            const translateAmount = (window.pageYOffset * i) / children.length
+            children[i].style.transform = `translateY(-${translateAmount}px)`
           }
         },
-        false
+        getPassiveEventConfig()
       )
     }
-    // setItemParallax(event) {
-    //   // documentation: https://github.com/dixonandmoe/rellax
-    //   // new Rellax(event.target.parentElement, {
-    //   //   vertical: true,
-    //   //   horizontal: false
-    //   // })
-    // }
   }
 })
 </script>
@@ -89,41 +80,41 @@ $header-width: 56px;
 .wrapper {
   margin-top: rem($header-width);
 }
-#parallax-container {
-  height: rem(1392px);
+.parallax-container {
+  height: rem(1600px);
   display: block;
 }
-#parallax-container > .layer {
+.parallax-container > .layer {
   position: fixed;
   transform: translateY(0px);
-  height: rem(1392px);
+  height: rem(1600px);
   width: 100%;
-  background-repeat: no-repeat !important;
+  background-repeat: no-repeat;
   background-position-x: center;
 }
 .layer-1 {
-  background-position-y: top !important;
+  background-position-y: top;
   top: rem($header-width);
 }
 .layer-2 {
-  background-position-y: top !important;
+  background-position-y: top;
   top: rem($header-width);
 }
 .layer-3 {
-  background-position-y: top !important;
+  background-position-y: top;
   top: rem($header-width + 80px);
 }
 .layer-4 {
-  background-position-y: bottom !important;
-  top: rem($header-width - 280px);
+  background-position-y: top;
+  top: rem($header-width + 390px);
 }
 .layer-5 {
-  background-position-y: bottom !important;
-  top: rem($header-width + 10px);
+  background-position-y: top;
+  top: rem($header-width + 550px);
 }
 .layer-6 {
-  background-position-y: bottom !important;
-  top: rem($header-width + 10px);
+  background-position-y: top;
+  top: rem($header-width + 960px);
 }
 .headerText {
   top: 137px;
