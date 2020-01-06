76d9007888b58c37213df6ba0a8ec326c1f97e95
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-01-06 23:18:44 +0330

    minors - fix resort-description markdown-content style, add search page margin-top #291

--- src/components/searchpage.vue
+++ src/components/searchpage.vue
@@ -1,6 +1,6 @@
 <template>
 <!-- TODO: move to views/ -->
-  <div>
+  <div class="page-search">
     <v-container class="is-limited" grid-list-md style="min-height:100vh;">
       <v-flex xs12 class="headerText">
         <h1>Results for {{id}}</h1>
@@ -67,6 +67,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$header-height: 56px;
+.page-search {
+  margin-top: rem($header-height);
+}
   .card{
     box-shadow: 0px 9px 24px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 10px;
