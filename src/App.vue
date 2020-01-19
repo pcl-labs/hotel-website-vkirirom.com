10ff9bf1a76950bfc76c7b1b8308e58e8289a0a8
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-01-19 23:17:25 +0330

    page-header - make header visible by scroll in homepage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <v-app class="wrapper" :class="hiddenLanguagesClasses" id="app">
-    <page-header></page-header>
     <!-- TODO: remove :key similar to https://github.com/whynotearth/shinta-mani-wild/pull/298 -->
     <router-view :key="$route.name + ($route.params.id || '')" />
   </v-app>
@@ -12,9 +11,6 @@ const PageHeader = () => import('@/components/PageHeader.vue')
 
 export default {
   name: 'app',
-  components: {
-    PageHeader
-  },
   created() {
     this.getUser()
   },
