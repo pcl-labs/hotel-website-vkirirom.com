acc39f6baff1b7b9205b9104513cf752d26f027f
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-01-31 20:25:51 +0330

    Add notification for new version (#378)
    * pwa - add notification for new version

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,6 +1,24 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker'
+import store from './store'
+
+const notifyUserAboutUpdate = (worker: any) => {
+  store.dispatch('snackbar/show', {
+    text: 'New version of site is available!',
+    timeout: 120000,
+    color: 'success',
+    class: '',
+    button: {
+      text: 'Refresh',
+      action: () => {
+        worker.postMessage({ type: 'SKIP_WAITING' })
+      },
+      color: 'white',
+      class: 'dark--text text-transform-none'
+    }
+  })
+}
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
@@ -18,8 +36,9 @@ if (process.env.NODE_ENV === 'production') {
     updatefound() {
       console.log('New content is downloading.')
     },
-    updated() {
+    updated(registration) {
       console.log('New content is available; please refresh.')
+      notifyUserAboutUpdate(registration.waiting)
     },
     offline() {
       console.log('No internet connection found. App is running in offline mode.')
@@ -27,5 +46,15 @@ if (process.env.NODE_ENV === 'production') {
     error(error) {
       console.error('Error during service worker registration:', error)
     }
+  })
+
+  let refreshing
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    console.log('controllerchange...')
+    if (refreshing) {
+      return
+    }
+    window.location.reload()
+    refreshing = true
   })
 }
--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,10 @@ const webpack = require('webpack')
 module.exports = {
   transpileDependencies: ['vuex-persist', 'vuetify'],
   pwa: {
+    name: require('./package.json').name,
     workboxOptions: {
-      skipWaiting: true,
-      clientsClaim: true
+      skipWaiting: true
+      // clientsClaim: true
     }
   },
   css: {
